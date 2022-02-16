03b7604a5c44b90032d1a37141f374dd17a02c0b
alcotta <69979848+alcotta@users.noreply.github.com>
2022-02-16 14:27:41 -0500

    Add files via upload

---
+++ Cookie Recipe Batch Editor.py
@@ -0,0 +1,20 @@
+originalBatch = 48 
+originalSugar = 1.5
+originalButter = 1.0
+originalFlour = 2.75
+
+batchSize = float ( input("Enter the desired number of cookies: ") ) \
+
+apsSugar = originalSugar / originalBatch 
+apsButter = originalButter / originalBatch
+apsFlour = originalFlour / originalBatch
+
+newSugar = apsSugar * batchSize
+newButter = apsButter * batchSize
+newFlour = apsFlour * batchSize
+
+print ( "Number of cups of flour:" + str(newFlour)) 
+print ( "Number of cups of butter:" + str(newButter)) 
+print ( "Number of cups of sugar:" + str(newSugar))
+
+print ("Amanda Alcott-Herr")
